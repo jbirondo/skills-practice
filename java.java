14e3e18fdb792dfb2bf4085fd8bd2e4e28134be3
Eugene Birondo <jbirondo@gmail.com>
2023-05-03 23:15:30 -0700

    two sum java

--- java.java
+++ java.java
@@ -36,3 +36,18 @@ class Solution {
     }
 }
 
+import java.util.HashMap;
+import java.util.Map;
+ 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numToIndex.containsKey(target - nums[i])) {
+                return new int[] {numToIndex.get(target - nums[i]), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
