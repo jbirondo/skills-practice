fe7ad7f9341894cfb209857a2410ede1db07e736
Eugene Birondo <jbirondo@gmail.com>
2023-05-03 23:17:03 -0700

    two sum python

---
+++ python.py
@@ -0,0 +1,19 @@
+# two-sum
+# brute force
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (i != j and nums[i] + nums[j] == target):
+                    return [i, j]
+        return []
+    
+# optimized with hash
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+        return []
