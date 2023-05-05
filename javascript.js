1478a553f5f1520478f03f336c5f9d26a6ade2ba
Eugene Birondo <jbirondo@gmail.com>
2023-05-04 22:45:38 -0700

    Best Time to Buy and Sell Stock

--- javascript.js
+++ javascript.js
@@ -37,3 +37,20 @@ var twoSum = function (nums, target) {
 }
 
 // Best Time to Buy and Sell Stock
+
+const maxProfit = (prices) => {
+  let left = 0; // Buy
+  let right = 1; // sell
+  let max_profit = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left]; // our current profit
+
+      max_profit = Math.max(max_profit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return max_profit;
+};
