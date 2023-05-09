33b57f6c7acd171ea8f19c6e8cea61a9246b49a7
Eugene Birondo <jbirondo@gmail.com>
2023-05-08 23:11:25 -0700

    contains dup

--- java.java
+++ java.java
@@ -109,3 +109,25 @@ class Solution {
         return false;
     }
 }
+
+// traverse through each element the param array
+// Time complexity: O(n)
+// Space complexity: O(n)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // Base case...
+        if(nums==null || nums.length==0)
+            return false;
+        // Create a hashset...
+        HashSet<Integer> hset = new HashSet<Integer>();
+        // Traverse all the elements through the loop...
+        for(int idx: nums){
+            // If it contains duplicate...
+            if(!hset.add(idx)){
+                return true;
+            }
+        }
+        // Otherwise return false...
+        return false;
+    }
+}
