1a045af24365958eae1521dc565b455935c05ba0
Eugene Birondo <jbirondo@gmail.com>
2023-05-09 23:11:29 -0700

    238. Product of Array Except Self

--- python.py
+++ python.py
@@ -96,3 +96,21 @@ class Solution:
         result = [left_product[i] * right_product[i] for i in range(n)]
 
         return result
+    
+# optimized
+
+class Solution:
+      def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [1] * n
+        # calculate the product of elements to the left of each element and store in result
+        for i in range(1, n):
+            result[i] = result[i - 1] * nums[i - 1]
+
+        # calculate the product of elements to the right of each element and update result
+        right_product = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= right_product
+            right_product *= nums[i]
+
+        return result
