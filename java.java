93c58c27864894ebfd29ef61fabdf65bdb1dce3e
Eugene Birondo <jbirondo@gmail.com>
2023-05-09 23:08:47 -0700

    238. Product of Array Except Self

--- java.java
+++ java.java
@@ -178,3 +178,24 @@ class Solution {
         return ans;
     }
 }
+
+// directly storing final product of prefix and suffix
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans, 1);
+        int curr = 1;
+        for(int i = 0; i < n; i++) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] *= curr;
+            curr *= nums[i];
+        }
+        return ans;
+    }
+}
