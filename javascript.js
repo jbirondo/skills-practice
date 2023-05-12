964a1529bbcd81d854e4d57d38c57ed162fd21eb
Eugene Birondo <jbirondo@gmail.com>
2023-05-11 22:59:30 -0700

    153. Find Minimum in Rotated Sorted Array

--- javascript.js
+++ javascript.js
@@ -188,3 +188,14 @@ var maxSubArray = function(nums) {
 };
 
 // 153. Find Minimum in Rotated Sorted Array
+
+const findMin = (nums) => {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const m = ~~((l + r) / 2);
+    if (nums[m] > nums[r]) l = m + 1;
+    else r = m;
+  }
+  return nums[l];
+};
