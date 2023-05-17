5170ac8720b9e7b86eabcbaa033c047bed219717
Eugene Birondo <jbirondo@gmail.com>
2023-05-16 22:12:14 -0700

    33. Search in Rotated Sorted Array

--- python.py
+++ python.py
@@ -172,3 +172,13 @@ class Solution:
             return self.helper(nums, l, mid)
         
 # 33. Search in Rotated Sorted Array
+
+def search(self, nums, target):
+    lo, hi = 0, len(nums) - 1
+    while lo < hi:
+        mid = (lo + hi) / 2
+        if (nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]):
+            lo = mid + 1
+        else:
+            hi = mid
+    return lo if target in nums[lo:lo+1] else -1
