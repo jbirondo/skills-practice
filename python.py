5a4ed962e9e976ec2cca1f38391a06c17e278f7a
Eugene Birondo <jbirondo@gmail.com>
2023-05-18 23:38:54 -0700

    11. Container With Most Water

--- python.py
+++ python.py
@@ -278,3 +278,16 @@ class Solution:
                     j -= 1
                 if res > ans: ans = res
             return ans
+        
+    # Brute force
+    
+    class Solution(object):
+        def maxArea(self, height):
+            max_container = 0
+            for p1 in range(len(height)):
+                for p2 in range(p1+1,len(height)):
+                    heights = min(height[p1],height[p2])
+                    width = p2 - p1
+                    container = heights * width
+                    max_container = max(max_container, container)
+            return max_container
