f42598700e7d26171fb13fc824224aab7968fc9c
Eugene Birondo <jbirondo@gmail.com>
2023-05-22 23:36:48 -0700

    70. Climbing Stairs

--- java.java
+++ java.java
@@ -407,3 +407,20 @@ public int climbStairs(int n) {
     }
     return mem[n-1];
 }
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+        
+        if(n==2) return 2;
+
+        int[] a =  new int[n];
+        a[0]=1;
+        a[1]=2;
+
+        for(int i=2;i<n;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        return a[n-1];
+    }
+}
