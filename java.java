04839630472cdcdfa19276825511af4694d7bba9
Eugene Birondo <jbirondo@gmail.com>
2023-05-22 23:37:14 -0700

    70. Climbing Stairs

--- java.java
+++ java.java
@@ -424,3 +424,22 @@ class Solution {
         return a[n-1];
     }
 }
+
+/**
+ * Question   : Climbing Stairs
+ * Complexity : Time: O(2^n) ; Space: O(n)
+ */
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
