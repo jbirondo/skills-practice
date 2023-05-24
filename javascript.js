d67bd35ee97a05f4d59c7f6e5432732793fe048d
Eugene Birondo <jbirondo@gmail.com>
2023-05-23 23:52:13 -0700

    322. Coin Change

--- javascript.js
+++ javascript.js
@@ -552,3 +552,16 @@ var coinChange = function(coins, amount) {
     const result = permute(amount);
     return result === Infinity ? -1 : result;
 };
+
+// tabulation
+var coinChange = function(coins, amount) {
+    const dp = Array(amount+1).fill(Infinity);
+    dp[0] = 0;
+    
+    for(let i = 1; i < dp.length; i++) {
+        for(let coin of coins) {
+            if(i-coin >= 0) dp[i] = Math.min(dp[i], dp[i-coin]+1);
+        }
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount];
+};
