8a70544b3ee98c0b6748f1e54dac74769464df54
Eugene Birondo <jbirondo@gmail.com>
2023-05-24 23:44:48 -0700

    300. Longest Increasing Subsequence

--- javascript.js
+++ javascript.js
@@ -598,3 +598,13 @@ var lengthOfLIS = function(nums) {
     
     return 0;
 };
+
+const lengthOfLIS = nums => {
+  let sequence = Array(nums.length).fill(1);
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (nums[j] < nums[i]) sequence[i] = Math.max(sequence[i], sequence[j] + 1);
+    }
+  }
+  return Math.max(...sequence)
+}
