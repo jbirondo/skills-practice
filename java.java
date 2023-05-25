a2c0b9cadcb744df77f77dac0202b782b080e427
Eugene Birondo <jbirondo@gmail.com>
2023-05-24 23:34:58 -0700

    300. Longest Increasing Subsequence

--- java.java
+++ java.java
@@ -489,3 +489,20 @@ public class Solution {
 
 // 300. Longest Increasing Subsequence
 
+public int lengthOfLIS(int[] nums) {
+    int[] tails = new int[nums.length];
+    int size = 0;
+    for (int x : nums) {
+        int i = 0, j = size;
+        while (i != j) {
+            int m = (i + j) / 2;
+            if (tails[m] < x)
+                i = m + 1;
+            else
+                j = m;
+        }
+        tails[i] = x;
+        if (i == size) ++size;
+    }
+    return size;
+}
