70f6f77600c867d2e5576edbfae52bb4ee228e8c
Eugene Birondo <jbirondo@gmail.com>
2023-05-24 23:41:07 -0700

    300. Longest Increasing Subsequence

--- python.py
+++ python.py
@@ -398,4 +398,14 @@ def lengthOfLIS(self, nums):
         size = max(i + 1, size)
     return size
 
-# Runtime: 48 ms
+# brute force
+
+def lengthOfLIS(self, nums: List[int]) -> int:
+        def max_lis(idx, cur_max):
+            if idx == len(nums):
+                return 0
+            if nums[idx] > cur_max:
+                return max(1 + max_lis(idx + 1, nums[idx]), max_lis(idx + 1, cur_max))
+            return max_lis(idx + 1, cur_max)
+    
+	return max_lis(0, float('-inf'))
