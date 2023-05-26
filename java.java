4546b6e9291e340f8557a3b149ccd3c038d1b518
Eugene Birondo <jbirondo@gmail.com>
2023-05-25 22:58:29 -0700

    139. Word Break

--- java.java
+++ java.java
@@ -588,3 +588,18 @@ private boolean wordBreak(int p, String s, Set<String> dict){
     }
     return mem[p]=false;
 }
+
+// iterative dp
+
+public boolean wordBreak(String s, List<String> wordDict) {
+    int n=s.length();
+    boolean[] dp=new boolean[n+1];
+    dp[n]=true;
+    Set<String> dict=new HashSet(wordDict);
+    for(int i=n-1;i>=0;i--) {
+        for(int j=i+1;!dp[i] && j<=n;j++) { 
+            dp[i] = dp[j] & dict.contains(s.substring(i,j));
+        }
+    }
+    return dp[0];   
+}
