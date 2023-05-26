1ced9852d6b42dad08cb410c3cc1657be52427ca
Eugene Birondo <jbirondo@gmail.com>
2023-05-25 23:02:59 -0700

    139. Word Break

--- python.py
+++ python.py
@@ -451,3 +451,10 @@ def lengthOfLIS(self, nums: List[int]) -> int:
 
 # 139. Word Break
 
+def wordBreak(self, s, words):
+    ok = [True]
+    max_len = max(map(len,words+['']))
+    words = set(words)
+    for i in range(1, len(s)+1):
+        ok += any(ok[j] and s[j:i] in words for j in range(max(0, i-max_len),i)),
+    return ok[-1]
