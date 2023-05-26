f2869c364aee846b6171d4045a43573770f6492c
Eugene Birondo <jbirondo@gmail.com>
2023-05-25 22:56:57 -0700

    139. Word Break

--- java.java
+++ java.java
@@ -548,3 +548,21 @@ public class Solution {
 
 // 139. Word Break
 
+// brute force
+
+public boolean wordBreak(String s, List<String> wordDict) {    
+    return wordBreak(0,s,new HashSet(wordDict));   
+}
+private boolean wordBreak(int p, String s, Set<String> dict){
+    int n=s.length();
+    if(p==n) {
+        return true;
+    }
+    for(int i=p+1;i<=n;i++) {
+        if(dict.contains(s.substring(p,i)) && wordBreak(i,s,dict)) {
+            return true;
+        }
+    }
+    return false;
+}
+
