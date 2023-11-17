1f0cba55dc9c0e2c2114daf48cd467bd65cd51f6
Eugene Birondo <jbirondo@gmail.com>
2023-11-17 14:22:47 -0800

    blind 75 js

--- blindjs.js
+++ blindjs.js
@@ -1,4 +1,14 @@
-// two sum brute force
+// two sum brute force o(n^2)
+var twoSum = function(nums, target) {
+    let indices
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target) indicies = [i, j]
+        }
+    }
+    return indicies
+};
+// two sum with o(n) with cache
 var twoSum = function(nums, target) {
     let indicies = {}
     for (let i = 0; i < nums.length; i++){
