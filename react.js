cff25b7982769df26f202e5cf6713b0ef6340b7b
Eugene Birondo <jbirondo@gmail.com>
2023-11-18 10:53:20 -0800

    blind 75 js

--- react.js
+++ react.js
@@ -24,3 +24,50 @@ function App() {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/**
+  Challenge: Make the button functional
+  A click on button should toggle (show/hide) the string `Toggle Challenge` each time it is pressed
+  
+  Solution: https://codepen.io/angelo_jin/pen/abLwyrL
+  Video for reference: https://youtu.be/VzNNjNmbXpY
+**/
+function App() {
+  const [on, setOn] = React.useState(true);
+
+  return (
+    <>
+      <button onClick={
+          () => setOn(!on)
+        }>Hide Element Below</button>
+
+      {on && <div>Toggle Challenge</div>}
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+/** 
+  Challenge: User should be able to type in any characters on input and those character should show in the browser.
+  
+  Solution: https://codepen.io/angelo_jin/pen/yLzvMop
+  Video for reference: https://youtu.be/VzNNjNmbXpY
+**/
+function App() {
+  const [value, setValue] = React.useState("");
+
+  return (
+    <>
+      <input 
+        type="text" 
+        placeholder="Enter Text" 
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <p>{value}</p>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
