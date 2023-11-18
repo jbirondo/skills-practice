a345a866b74695fe7e075f03b561bc5e992cbfe8
Eugene Birondo <jbirondo@gmail.com>
2023-11-18 12:00:07 -0800

    React interview questions

--- react.js
+++ react.js
@@ -234,3 +234,61 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const url = "https://jsonplaceholder.typicode.com/users/1";
+/** 
+  // Sample Response
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org"
+  }
+**/
+
+/** 
+  Challenge: Given the url above, make this app fetch the data and display them in the browser (Small Hint: use `React.useEffect and fetch api`)
+  
+  Solution: https://codepen.io/angelo_jin/pen/zYEWZdW
+  Video for reference: https://youtu.be/VzNNjNmbXpY
+**/
+function App() {
+  const [userData, setUserData] = React.useState({});
+  
+  const getUserData = async () => {//create the async call to the api for userData
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    setUserData(jsonData)//set the userData variable using the setter
+  }
+  
+  React.useEffect(() => {
+    getUserData()//calls the api
+  }, [])
+  
+  // No need to touch code below
+  return (
+    <div className="App">
+      <h2>User Data</h2>
+      <p>
+        <strong>Name: </strong>{" "}
+        {userData.name || "(Need to populate name here)"}
+      </p>
+      <p>
+        <strong>Website: </strong>
+        {userData.website || "(Need to populate website here)"}
+      </p>
+      <p>
+        <strong>Email: </strong>
+        {userData.email || "(Need to populate email here)"}
+      </p>
+      <p>
+        <strong>Phone: </strong>
+        {userData.phone || "(Need to populate phone here)"}
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
