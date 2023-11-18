85b28b84b6d0120034335b4bb2aef2fa3cb773cd
Eugene Birondo <jbirondo@gmail.com>
2023-11-17 19:31:06 -0800

    blind 75 js

--- blindjs.js
+++ blindjs.js
@@ -18,3 +18,96 @@ var twoSum = function(nums, target) {
         indicies[nums[i]] = i
     }
 };
+// max profit two pointer
+var maxProfit = function(prices) {
+    let max = 0;
+    let p_one = prices[0];
+    let p_two = prices[0];
+    for (let i = 0; i < prices.length; i++){
+        if (prices[i] > p_two){
+            p_two = prices[i];
+            max = Math.max(max, p_two - p_one)
+        } else if ( prices[i] < p_one) {
+            p_one = prices[i];
+            p_two = prices[i];
+        }
+    }
+    return max
+};
+// contains duplicate using a hash
+var containsDuplicate = function(nums) {
+    let obj = {};
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] in obj){
+            return true
+        }
+        obj[nums[i]] = i
+    }
+    return false
+};
+// productExceptSelf using forward and reverse pass
+var productExceptSelf = function(nums) {
+    forward_pass = Array(nums.length).fill(1);
+    reverse_pass = Array(nums.length).fill(1);
+    forward_value = 1
+    reverse_value = 1
+    res = []
+    for (let i = 0; i < nums.length; i++){
+        forward_value = nums[i] * forward_value;
+        forward_pass[i] = forward_value
+    }
+    for (let i = nums.length - 1; i >= 0; i--){
+        reverse_value = nums[i] * reverse_value;
+        reverse_pass[i] = reverse_value
+    }
+    forward_pass.unshift(1)
+    forward_pass.push(1)
+    reverse_pass.unshift(1)
+    reverse_pass.push(1)
+    for (let i = 1; i < nums.length + 1; i++){
+        res.push(forward_pass[i - 1] * reverse_pass[i + 1])
+    }
+    return res
+};
+// maximum subarray o(n) time o(1) space
+var maxSubArray = function(nums) {
+    currentSum = 0;
+    finalSum = Math.max(...nums)
+    for (let i = 0; i < nums.length; i++){
+        currentSum += nums[i]
+        if (currentSum < 0){
+            currentSum = 0
+        } else {
+            finalSum = Math.max(finalSum, currentSum)
+        }
+    }
+    return finalSum
+};
+// maximum product subarray dp approach
+var maxProduct = function(nums) {
+    let forward = Array(nums.length).fill(1);
+    max = Math.max(...nums);
+    currentSum = forward[0]
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 0){
+            currentSum = 1
+            continue
+        }
+        currentSum *= nums[i]
+        forward[i] = currentSum
+        max = Math.max(max, currentSum)
+    }
+    let reverse = Array(nums.length).fill(1);
+    currentSum = reverse[nums.length - 1]
+    for (let i = nums.length - 1; i >= 0; i-- ){
+        if (nums[i] == 0){
+            currentSum = 1
+            continue
+        }
+        currentSum *= nums[i]
+        reverse[i] = currentSum
+        max = Math.max(max, currentSum)
+    }
+    return max
+};
+// 
